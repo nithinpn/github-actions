03ca363278df71beb20bdf4465773cb1bc2e0e9e
nithinpn <116748738+nithinpn@users.noreply.github.com>
2023-07-23 15:35:34 +0530

    Update TestRunner.java

--- src/test/java/features/TestRunner.java
+++ src/test/java/features/TestRunner.java
@@ -2,10 +2,13 @@ package features;
 
 import com.intuit.karate.junit5.Karate;
 
-public class TestRunner {
-    @Karate.Test
-    Karate testTags() {
-       // return Karate.run("classpath:features/demo.feature","classpath:features/demo2.feature" ).relativeTo(getClass());
-        return Karate.run("classpath:features" ).tags("@sample").relativeTo(getClass());
+
+import com.intuit.karate.junit5.Karate;
+
+    public class TestRunner{
+        @Karate.Test
+        Karate run() {
+            // return Karate.run("classpath:features/demo.feature","classpath:features/demo2.feature" ).relativeTo(getClass());
+            return Karate.run("classpath:features" ).tags("@sample").relativeTo(getClass());
+        }
     }
-}
